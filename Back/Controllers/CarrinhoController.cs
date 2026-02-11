ca175bbfc90a74c75a5125fbd5cff518bbec1e35
TeixeiraSantoss <128059526+TeixeiraSantoss@users.noreply.github.com>
2026-02-11 10:01:28 -0300

    chore: Carrinho controller, service e interface criados

---
+++ Back/Controllers/CarrinhoController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Controllers
+{
+    [ApiController]
+    [Route("api/carrinho")]
+    public class CarrinhoController : ControllerBase
+    {
+        public CarrinhoController()
+        {
+            
+        }
+    }
+}
--- Back/Program.cs
+++ Back/Program.cs
@@ -1,3 +1,6 @@
+using Back.Interface;
+using Back.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Configuração do BD
@@ -11,6 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Injetando dependencia nas Interfaces
+builder.Services.AddScoped<ICarrinhoService, CarrinhoService>();
 
 var app = builder.Build();
 
